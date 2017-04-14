b04f26e335aab2b3e6b567d2bb577cac92d63c20
Justice Ampofo <justiceampofo@gmail.com>
2017-04-14 19:19:10 -0400

    Centred student content for larger screens

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,7 +1,7 @@
 <nav class="menu <?php if($page_title == 'Home'){echo 'fixed';} ?>">
   <ul class="nav-logos">
     <li><a class="<?php if($page_title == 'Home'){echo 'scroll';} ?>" id="logo" href="<?php if($page_title == 'Home'){echo '#home';} else{echo 'index.php#home';} ?>"><span class="hide">logo</span></a></li>
-    <li class="<?php if($page_title == 'Home'){echo 'hide-logo-text';} ?>"><a class="<?php if($page_title == 'Home'){echo 'scroll';} ?>" id="logo-type" href="<?php if($page_title == 'Home'){echo '#home';} else{echo 'index.php#home';} ?>"><h1 class="all-caps">The Final Draft</h1></a></li>
+    <li class="<?php if($page_title == 'Home'){echo 'hide-logo-text';} ?>"><a class="<?php if($page_title == 'Home'){echo 'scroll';} ?>" id="logo-type" href="<?php if($page_title == 'Home'){echo '#home';} else{echo 'index.php#home';} ?>"><span class="hide">The Final Draft</span></a></li>
     <!-- <li><a href="#" id="menu_mobile"><i class="fa fa-bars"></i></a></li> -->
   </ul>
 
